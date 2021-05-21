fad7de2240d3025af102d71281f3ad852f1e6daf
Daniel Castiblanco <razzziel001@gmail.com>
2021-05-21 18:09:00 -0500

    creando primeros tags

---
+++ mainBot.py
@@ -0,0 +1,38 @@
+import  nltk
+from nltk.stem.lancaster import LancasterStemmer
+stemmer = LancasterStemmer
+import numpy
+import tflearn
+import tensorflow
+import json
+import random
+import pickle
+
+# nltk.download('punkt')
+
+with open("contenido.json") as archivo:
+    datos = json.load(archivo)
+    
+palabras=[]
+tags=[]
+auxX=[]
+auxY=[]
+
+for contenido in datos["contenido"]:
+    for patrones in contenido["patrones"]:
+        #Esta función de word_tokenize() separa como split y reconoce caracteres especiales
+        auxPalabra = nltk.word_tokenize(patrones)
+        palabras.extend(auxPalabra)
+        auxX.append(auxPalabra)
+        auxY.append(contenido["tag"])
+        
+        if contenido["tag"] not in tags:
+            tags.append(contenido["tag"])
+        
+print(palabras)
+print(auxY)
+print(auxX)
+print(tags)        
+        
+
+
