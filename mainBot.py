938802595652b57d1d1fb25d27b319974d8eefdf
Daniel Castiblanco <razzziel001@gmail.com>
2021-05-25 22:47:58 -0500

    funciones de tflearn para generar las redes neuronales

--- mainBot.py
+++ mainBot.py
@@ -1,9 +1,10 @@
 import  nltk
 from nltk.stem.lancaster import LancasterStemmer
-stemmer = LancasterStemmer
+stemmer = LancasterStemmer()
 import numpy
 import tflearn
 import tensorflow
+from tensorflow.python.framework import ops
 import json
 import random
 import pickle
@@ -29,10 +30,41 @@ for contenido in datos["contenido"]:
         if contenido["tag"] not in tags:
             tags.append(contenido["tag"])
         
-print(palabras)
-print(auxY)
-print(auxX)
-print(tags)        
+palabras = [stemmer.stem(w.lower()) for w in palabras if w!="?"]
+palabras = sorted(list(set(palabras)))     
+tags = sorted(tags)
+
+entrenamiento = []
+salida = []
+salidVacia = [0 for _ in range(len(tags))]
+
+for x, documento in enumerate(auxX):
+    cubeta=[]
+    auxPalabra = [stemmer.stem(w.lower()) for w in documento]
+    for w in palabras:
+        if w in auxPalabra:
+            cubeta.append(1)
+        else:
+            cubeta.append(0)
+    filaSalida = salidVacia[:]
+    filaSalida[tags.index(auxY[x])] = 1
+    entrenamiento.append(cubeta)
+    salida.append(filaSalida)
+
+entrenamiento = numpy.array(entrenamiento)
+salida = numpy.array(salida)
+
+ops.reset_default_graph()
+red = tflearn.input_data(shape=[None,len(entrenamiento[0])])
+red = tflearn.fully_connected(red, 10)
+red = tflearn.fully_connected(red, 10)
+red = tflearn.fully_connected(red, len(salida[0]),activation="softmax")
+red = tflearn.regression(red) 
+
+modelo = tflearn.DNN(red)
+modelo.fit(entrenamiento, salida, n_epoch = 1000, batch_size = 10, show_metric = True)
+modelo.save("modelo.tflearn")            
+    
         
 
 
